595cd86e74ccc2c5b3e807b31c69e7dbcc285b74
AshKemp <kmuraliashwin@gmail.com>
2024-10-20 11:41:44 +0530

    install tanstack query

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import "./App.css";
 import Weather from "./components/Weather";
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Weather />
-    </>
+    </QueryClientProvider>
   );
 }
 
